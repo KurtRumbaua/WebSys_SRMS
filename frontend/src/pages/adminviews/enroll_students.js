bac93cad88b883c351d47a1bc60944682cd7cf7a
KurtRumbaua <kurtrumbaua28@gmail.com>
2024-05-10 07:26:33 +0800

    employees, Enrollment, Financial management pages

--- frontend/src/pages/adminviews/enroll_students.js
+++ frontend/src/pages/adminviews/enroll_students.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import "../../styles/enroll-students.css"; // Make sure to import the CSS file
 import logo from "../../assets/Schoollogo.png";
 import { NavLink } from "react-router-dom";
 
 function EnrollStudents() {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [selectedEnrollee, setSelectedEnrollee] = useState(null);
+
+  // Function to toggle pop-up visibility and set selected enrollee
+  const togglePopup = (enrollee) => {
+    setSelectedEnrollee(enrollee);
+    setIsPopupOpen(!isPopupOpen);
+  };
   return (
     <>
       <div className="admin-container">
@@ -53,7 +61,7 @@ function EnrollStudents() {
           </thead>
           <tbody>
             {/* Example row (you can add more or integrate with data fetching logic) */}
-            <tr>
+            <tr onClick={() => togglePopup({ id: 1, lastName: "Lorem Ipsum", firstName: "Lorem Ipsum", field: "Lorem" })}>
               <td>1</td>
               <td>Lorem Ipsum</td>
               <td>Lorem Ipsum</td>
@@ -65,6 +73,41 @@ function EnrollStudents() {
         </table>
       </div>
       </div>
+      {isPopupOpen && (
+        <div className="admin-popup-container">
+          <div className="admin-popup-content">
+            <h2>Enrollee Details</h2>
+            <p>Last Name:</p>
+            <p>First Name:</p>
+            <p>Grade Level:</p>
+            <p>Birth Date:</p>
+            <p>Contact No:</p>
+            <p>Email:</p>
+            <p>Address:</p>
+            <p>Enrollee Type: </p>
+            <p>Form138:</p>
+            <p>Birth Certificate: </p>
+            <p>GMC</p>
+            <p>Proof of Payment</p>
+            <div className="form-group">
+              <label htmlFor="section">Section:</label>
+              <select id="section" name="section">
+              <option value="imus">Incoming</option>
+              <option value="dasmarinas">Outgoing</option>
+              </select>
+          </div>
+            <div className="form-group">
+              <label htmlFor="enrollmentstatus">In Progress:</label>
+              <select id="enrollmentstatus" name="enrollmentstatus">
+              <option value="inprogress">In-PRogress</option>
+              <option value="accepted">Accepted</option>
+              </select>
+          </div>
+            <button>Confirm</button>
+            <button onClick={() => togglePopup(null)}>Close</button>
+          </div>
+        </div>
+      )}
       <div>
         <footer className="admin-footer">
           <p>Sta. Teresita Elementary School © 2023. All Rights Reserved.</p>
