42d4783ee5bc4536aa6e51b1716666a6155fe70c
gnr2 <ellisdavid023@gmail.com>
2024-05-10 03:29:36 +0800

    Add Admin Enroll Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+//teacher-views
 import ClassManagementPage from "./pages/teacherviews/class-management";
 import StudentRosterPage from "./pages/teacherviews/student-roster";
 import TeacherDashboard from "./pages/teacherviews/teacher-views";
 import TeachersPage from "./pages/teacherviews/teacher-roster";
+
+//auth pages
 import Auth from "./pages/authpages/Auth";
-import StudenInfo from "./pages/parentviews/studentinfo";
 import Login from "./pages/authpages/Login";
+
+//admin views
+import EnrollStudentPage from "./pages/adminviews/enroll_students";
+
+//parent-views
+import StudenInfo from "./pages/parentviews/studentinfo";
 import ParentHome from "./pages/parentviews/parenthome";
+import EnrollmentPage from "./pages/parentviews/enrollment";
 
 function App() {
   return (
@@ -19,8 +29,10 @@ function App() {
         <Route path="/teachers" element={<TeachersPage />} />
         <Route path="/" element={<Navigate to="/auth" />} />
         <Route path="/auth" element={<Auth />} />
-        <Route path="/parenthome" element={<ParentHome/>}/>
-        <Route path="/studentprofile" element={<StudenInfo/>}/>
+        <Route path="/parenthome" element={<ParentHome />} />
+        <Route path="/studentprofile" element={<StudenInfo />} />
+        <Route path="/enrollment" element={<EnrollmentPage />} />
+        <Route path="enroll-student" element={<EnrollStudentPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<Navigate to="/auth" />} />
       </Routes>
