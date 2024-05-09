5779020af1291eedd62a50cb0695b1275ddf63d4
KurtRumbaua <kurtrumbaua28@gmail.com>
2024-05-10 02:23:35 +0800

    deleted, and transferred pages from teacherviews

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import ClassManagementPage from "./pages/teacherviews/class-management";
+import ClassManagementPage from "./pages/adminviews/class-management";
 import StudentRosterPage from "./pages/teacherviews/student-roster";
-import TeacherDashboard from "./pages/teacherviews/teacher-views";
-import TeachersPage from "./pages/teacherviews/teacher-roster";
+import TeacherClasses from "./pages/teacherviews/teacher-classes";
+import TeachersPage from "./pages/dump/teacher-roster";
 import Auth from "./pages/authpages/Auth";
 import StudenInfo from "./pages/parentviews/studentinfo";
 import Login from "./pages/authpages/Login";
@@ -16,7 +16,7 @@ function App() {
       <Routes>
         <Route path="/class-management" element={<ClassManagementPage />} />
         <Route path="/student-roster" element={<StudentRosterPage />} />
-        <Route path="/teacher-dashboard" element={<TeacherDashboard />} />
+        <Route path="/teacher-classes" element={<TeacherClasses />} />
         <Route path="/teachers" element={<TeachersPage />} />
         <Route path="/" element={<Navigate to="/auth" />} />
         <Route path="/auth" element={<Auth />} />
